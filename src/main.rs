7648ca780ffe25a6d71ebdcab2163f61d3e732fa
bucky0112 <bucky0112@gmail.com>
2023-10-11 16:58:48 +0800

    Add new files: `.env.template`, `.gitignore`, `Cargo.toml`, `readme.md`, and `src/main.rs`. Implement a Rust Weather CLI tool that retrieves the current weather based on a city name input. The tool uses the Google Geocoding API to get the latitude and longitude of the city, and the OpenWeatherMap API to fetch the weather information

---
+++ src/main.rs
@@ -0,0 +1,82 @@
+use clap::Parser;
+use dotenv::dotenv;
+use std::env;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Opts {
+    #[clap(short, long)]
+    city: String,
+}
+
+#[derive(Debug)]
+enum MyError {
+    MissingField(String),
+    ReqwestError(reqwest::Error),
+}
+
+impl From<reqwest::Error> for MyError {
+    fn from(err: reqwest::Error) -> MyError {
+        MyError::ReqwestError(err)
+    }
+}
+
+async fn get_coordinates(city_name: &str) -> Result<(f64, f64), MyError> {
+    dotenv().ok();
+
+    let api_key = env::var("GOOGLE_MAPS_API_KEY").expect("GOOGLE_MAPS_API_KEY must be set");
+    let url = format!(
+        "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}",
+        city_name, api_key
+    );
+
+    let response: serde_json::Value = reqwest::get(&url).await?.json().await?;
+
+    let lat = response["results"][0]["geometry"]["location"]["lat"]
+        .as_f64()
+        .ok_or(MyError::MissingField(
+            "Latitude is missing or not a float".to_string(),
+        ))?;
+
+    let lng = response["results"][0]["geometry"]["location"]["lng"]
+        .as_f64()
+        .ok_or(MyError::MissingField(
+            "Longitude is missing or not a float".to_string(),
+        ))?;
+
+    Ok((lat, lng))
+}
+
+async fn process_city(city_name: &str) {
+    match get_coordinates(city_name).await {
+        Ok((lat, lng)) => match get_weather(lat as u32, lng as u32).await {
+            Ok((weather_description, temperature)) => {
+                println!("City: {}\nLatitude: {:.2}, Longitude: {:.2}\nCurrent Weather: {}\nTemperature: {:.2}°C", city_name, lat, lng, weather_description, temperature);
+            }
+            Err(e) => println!("Error: {:?}", e),
+        },
+        Err(e) => println!("Error: {:?}", e),
+    }
+}
+
+async fn get_weather(lat: u32, lon: u32) -> Result<(String, f64), MyError> {
+    dotenv().ok();
+
+    let api_key = env::var("OPEN_WEATHER_API_KEY").expect("OPEN_WEATHER_API_KEY must be set");
+    let url = format!("https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude=minutely,hourly,daily,alerts&units=metric&appid={}", lat, lon, api_key);
+
+    let response: serde_json::Value = reqwest::get(&url).await?.json().await?;
+    let weather_description = response["current"]["weather"][0]["description"]
+        .as_str()
+        .unwrap()
+        .to_string();
+
+    let temperature = response["current"]["temp"].as_f64().unwrap();
+    Ok((weather_description, temperature))
+}
+
+#[tokio::main]
+async fn main() {
+    let opts = Opts::parse();
+    process_city(&opts.city).await;
+}
